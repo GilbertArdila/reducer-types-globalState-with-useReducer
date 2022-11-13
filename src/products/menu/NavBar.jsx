cc0119a0fd66665d5f0e8ab27ab51a217128423a
Gilbert Ardila <gilbertferney@gmail.com>
2022-11-12 21:33:34 -0500

    arreglamos bug en cart

--- src/products/menu/NavBar.jsx
+++ src/products/menu/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = ({setShowPage,showPage}) => {
    
     {cart.length > 0 ? showPage ? <div className='cart' onClick={() => setShowPage(false)}><img src='https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_home_48px-64.png' alt='home' /></div>: <div className='cart'
     onClick={() => setShowPage(true)}
-    ><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-cart-outline-64.png" alt="cart" /></div>: null}
+    ><img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-ios7-cart-outline-64.png" alt="cart" /></div>:  showPage ?<div className='cart' onClick={() => setShowPage(false)}><img src='https://cdn0.iconfinder.com/data/icons/google-material-design-3-0/48/ic_home_48px-64.png' alt='home' /></div>:null}
     
     </nav>
   )
